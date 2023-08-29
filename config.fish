1b2c8a417e29704d2bdf8a703f373f650409a93b
ShadowTux <shadowtux@shadowoftux.tech>
2023-08-29 21:27:47 +0300

    fixed !!

--- config.fish
+++ config.fish
@@ -42,33 +42,15 @@ set fish_color_param brcyan
 ### FUNCTIONS ###
 
 # Functions needed for !! and !$
-function __history_previous_command
-  switch (commandline -t)
-  case "!"
-    commandline -t $history[1]; commandline -f repaint
-  case "*"
-    commandline -i !
-  end
+function sudo
+    if test "$argv" = !!
+        eval command sudo $history[1]
+    else
+        command sudo $argv
+    end
 end
 
-#function __history_previous_command_arguments
-#  switch (commandline -t)
-#  case "!"
-#    commandline -t ""
-#    commandline -f history-token-search-backward
-#  case "*"
-#    commandline -i '$'
-#  end
-#end
 
-# The bindings for !! and !$
-#if [ "$fish_key_bindings" = "fish_vi_key_bindings" ];
-#  bind -Minsert ! __history_previous_command
-#  bind -Minsert '$' __history_previous_command_arguments
-#else
-#  bind ! __history_previous_command
-#  bind '$' __history_previous_command_arguments
-#end
 
 # Function for creating a backup file
 # ex: backup file.txt
@@ -133,6 +115,8 @@ end
 #    \"))")
 #    printf $output
 #end
+
+
 
 ### END OF FUNCTIONS ###
 
